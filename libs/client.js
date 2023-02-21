549a466470daff234c6af04bd3c1ad6759f2418c
tgsmsk <tgsmsk0212@gmail.com>
2023-02-21 15:55:48 +0900

    client.jsの更新

---
+++ libs/client.js
@@ -0,0 +1,6 @@
+import { createClient } from "microcms-js-sdk";
+
+export const client = createClient({
+  serviceDomain: "chot-demo",
+  apiKey: process.env.kZ0AA9yAllIg7aAKXgL6I1RUr0Jfwt3bXT3P,
+});
