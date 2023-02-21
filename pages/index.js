1c234f892fd69b72c0fb36ddb3f408e99312a747
tgsmsk <tgsmsk0212@gmail.com>
2023-02-21 16:32:09 +0900

    ページを整えました

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,16 @@
 import Link from "next/link";
 import { client } from "../libs/client";
 
-export default function Home({ blog }) {
+export default function Home({ blog, category }) {
   return (
     <div>
+      <ul>
+        {category.map((category) => (
+          <li key={category.id}>
+            <Link href={`/category/${category.id}`}>{category.name}</Link>
+          </li>
+        ))}
+      </ul>
       <ul>
         {blog.map((blog) => (
           <li key={blog.id}>
@@ -18,10 +25,13 @@ export default function Home({ blog }) {
 // データをテンプレートに受け渡す部分の処理を記述します
 export const getStaticProps = async () => {
   const data = await client.get({ endpoint: "blog" });
+  // カテゴリーコンテンツの取得
+  const categoryData = await client.get({ endpoint: "categories" });
 
   return {
     props: {
       blog: data.contents,
+      category: categoryData.contents,
     },
   };
 };
